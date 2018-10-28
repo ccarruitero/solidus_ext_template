23a09abead9285d4dffb80688a292407881acb7b
Cesar Carruitero <cesar@mozilla.pe>
2018-10-28 17:25:57 -0500

    fix templates

--- template.rb
+++ template.rb
@@ -1,12 +1,11 @@
 source_paths.unshift(File.expand_path('../templates', __FILE__))
 
-# directory 'app', "#{name}/app"
+directory 'app'
 directory 'lib'
-# directory 'bin', "#{name}/bin"
-# directory 'config', "#{name}/config"
+directory 'config'
 
 template '%name%.gemspec'
-template 'Gemfile' # , "#{name}/Gemfile"
+template 'Gemfile'
 template 'gitignore', '.gitignore'
 template 'LICENSE'
 template 'Rakefile'
@@ -17,11 +16,11 @@ template 'rubocop.yml', '.rubocop.yml'
 template 'travis.yml', '.travis.yml'
 template 'rvmrc', '.rvmrc'
 
-after_bundle do
-  git :init
-  git add: '.'
-  git commit: "-a -m 'Initial commit'"
-end
+# after_bundle do
+#   git :init
+#   git add: '.'
+#   git commit: "-m 'Initial commit'"
+# end
 
 def source_paths
   [__dir__]
--- templates/lib/%namespaced_name%/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-<%= wrap_in_modules <<rb.strip_heredoc
-  VERSION = '0.1.0'
-rb
-%>
